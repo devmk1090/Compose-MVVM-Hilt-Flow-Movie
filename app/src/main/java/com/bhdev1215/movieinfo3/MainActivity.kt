5121b1111122b6f9b83347d2773ff826c26d798d
devmk1090 <devmk1090@gmail.com>
2023-01-19 03:28:04 +0900

    initial commit

---
+++ app/src/main/java/com/bhdev1215/movieinfo3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bhdev1215.movieinfo3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
