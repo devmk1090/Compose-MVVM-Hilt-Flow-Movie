9f4ea12c49d4dcb1f537e346948114dc4157edfa
devmk1090 <devmk1090@gmail.com>
2023-01-20 02:54:14 +0900

    Add gradle library and Success build

---
+++ app/src/main/java/com/bhdev1215/movieinfo3/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bhdev1215.movieinfo3.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
