9c51242e36e43840d06551f67c9843b964dc610f
devmk1090 <devmk1090@gmail.com>
2023-02-22 01:12:34 +0900

    Adding CastDetailScreen (4)

--- app/src/main/java/com/bhdev1215/movieinfo3/screens/components/CastDetailScreen.kt
+++ app/src/main/java/com/bhdev1215/movieinfo3/screens/components/CastDetailScreen.kt
@@ -1,15 +1,16 @@
 package com.bhdev1215.movieinfo3.screens.components
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -18,12 +19,14 @@ import coil.compose.rememberImagePainter
 import com.bhdev1215.movieinfo3.R
 import com.bhdev1215.movieinfo3.model.artist.CastDetail
 import com.bhdev1215.movieinfo3.model.artist.CastFilmography
+import com.bhdev1215.movieinfo3.navigation.NavigationObject
 import com.bhdev1215.movieinfo3.screens.detail.DetailViewModel
 import com.bhdev1215.movieinfo3.ui.theme.cornerRadius10
 import com.bhdev1215.movieinfo3.util.Constants
 import com.bhdev1215.movieinfo3.util.Resource
 import timber.log.Timber
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun CastDetailScreen(
     id: Int,
@@ -39,9 +42,10 @@ fun CastDetailScreen(
     }.value
 
     val data = castDetail.data
+    val castFilmographyList = castFilmography.data?.cast
 
     Box {
-        if (castDetail is Resource.Success) {
+        if (castDetail is Resource.Success && castFilmography is Resource.Success) {
             Column {
                 CommonAppBar(
                     title = {
@@ -78,6 +82,26 @@ fun CastDetailScreen(
                         }
                     }
                 }
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(
+                    text = "필모그래피",
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                LazyVerticalGrid(cells = GridCells.Fixed(2), content = {
+                    items(castFilmographyList!!.size) { it ->
+                        MovieItem(
+                            modifier = Modifier
+                                .height(285.dp)
+                                .clickable {
+                                },
+                            imageUrl = "${Constants.IMAGE_BASE_URL}/${castFilmographyList[it].posterPath}"
+                        )
+                    }
+                })
             }
         }
     }
