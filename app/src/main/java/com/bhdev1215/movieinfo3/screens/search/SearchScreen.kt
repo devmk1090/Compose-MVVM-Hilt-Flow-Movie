7072a39053f28c2219a4acdea82c8a8a26848529
devmk1090 <devmk1090@gmail.com>
2023-03-17 01:50:29 +0900

    Adding search screen (2). Implement navigation for search result items and Branch processing according to LoadState of Paging

--- app/src/main/java/com/bhdev1215/movieinfo3/screens/search/SearchScreen.kt
+++ app/src/main/java/com/bhdev1215/movieinfo3/screens/search/SearchScreen.kt
@@ -13,26 +13,34 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.bhdev1215.movieinfo3.R
 import com.bhdev1215.movieinfo3.model.Search
 import com.bhdev1215.movieinfo3.navigation.NavigationObject
 import com.bhdev1215.movieinfo3.screens.components.CommonAppBar
 import com.bhdev1215.movieinfo3.screens.components.MovieItem
 import com.bhdev1215.movieinfo3.ui.theme.cornerRadius10
+import com.bhdev1215.movieinfo3.ui.theme.primaryDark
 import com.bhdev1215.movieinfo3.ui.theme.primaryGray
 import com.bhdev1215.movieinfo3.util.Constants
+import retrofit2.HttpException
 import timber.log.Timber
+import java.io.IOException
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
 @Composable
@@ -80,16 +88,61 @@ fun SearchScreen(
                         modifier = Modifier
                             .height(185.dp)
                             .clickable {
-
+                                navController.navigate(NavigationObject.Detail.MOVIE_DETAIL.plus("/${searchResult[search]?.id}"))
                             },
                         imageUrl = "${Constants.IMAGE_BASE_URL}/${searchResult[search]?.posterPath}",
                         title = searchResult[search]?.title,
                         release = searchResult[search]?.releaseDate,
                         rating = searchResult[search]?.voteAverage.toString()
                     )
-                    Timber.d("searchResult : ${searchResult[search]?.title}")
                 }
             })
+
+            searchResult.apply {
+                when (loadState.refresh) {
+                    is LoadState.Loading -> {
+                        CircularProgressIndicator(
+                            modifier = Modifier.align(Alignment.Center),
+                            color = primaryDark,
+                        )
+                    }
+                    is LoadState.NotLoading -> {
+                        if (searchResult.itemCount == 0) {
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Image(
+                                    modifier = Modifier.size(200.dp),
+                                    painter = painterResource(id = R.drawable.ic_baseline_search_off_24),
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                    }
+                    is LoadState.Error -> {
+                        val e = searchResult.loadState.refresh as LoadState.Error
+                        Text(
+                            text = when (e.error) {
+                                is HttpException -> {
+                                    "HttpException !"
+                                }
+                                is IOException -> {
+                                    "Server error"
+                                }
+                                else -> {
+                                    "다시 시도해주세요"
+                                }
+                            },
+                            modifier = Modifier
+                                .align(alignment = Alignment.Center),
+                            textAlign = TextAlign.Center,
+                            color = Color.White
+                        )
+                    }
+                }
+            }
         }
     }
 }
